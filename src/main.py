206c4f46d4f19974246307d127b96ae359f8db9e
Andy Stewart <lazycat.manatee@gmail.com>
2012-09-27 17:37:37 +0800

    update.

---
+++ src/main.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 ~ 2012 Deepin, Inc.
+#               2011 ~ 2012 Wang Yong
+# 
+# Author:     Wang Yong <lazycat.manatee@gmail.com>
+# Maintainer: Wang Yong <lazycat.manatee@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from dtk.ui.init_skin import init_skin
+from dtk.ui.utils import get_parent_dir
+import os
+
+app_theme = init_skin(
+    "deepin-ui-demo", 
+    "1.0",
+    "01",
+    os.path.join(get_parent_dir(__file__, 2), "skin"),
+    os.path.join(get_parent_dir(__file__, 2), "app_theme"),
+    )
+
+from dtk.ui.application import Application
+from dtk.ui.constant import DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT
+
+from navigate_panel import NavigatePanel
+
+if __name__ == "__main__":
+    # Init application.
+    application = Application()
+
+    # Set application default size.
+    application.set_default_size(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT)
+
+    # Set application icon.
+    application.set_icon(app_theme.get_pixbuf("icon.ico"))
+    
+    # Set application preview pixbuf.
+    application.set_skin_preview(app_theme.get_pixbuf("frame.png"))
+    
+    # Add titlebar.
+    application.add_titlebar(
+        ["theme", "max", "min", "close"], 
+        app_theme.get_pixbuf("logo.png"), 
+        None,
+        "系统设置",
+        )
+    
+    # Add navigate panel.
+    navigate_panel = NavigatePanel()
+    application.main_box.pack_start(navigate_panel)
+    
+    application.run()
---
+++ src/navigate_panel.py
@@ -0,0 +1,244 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 ~ 2012 Deepin, Inc.
+#               2011 ~ 2012 Wang Yong
+# 
+# Author:     Wang Yong <lazycat.manatee@gmail.com>
+# Maintainer: Wang Yong <lazycat.manatee@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from dtk.ui.scrolled_window import ScrolledWindow
+from dtk.ui.iconview import IconView
+from dtk.ui.config import Config
+from dtk.ui.draw import draw_pixbuf, draw_text
+from dtk.ui.constant import DEFAULT_FONT_SIZE
+import pango
+import gtk
+import gobject
+import os
+
+class NavigatePanel(ScrolledWindow):
+    '''
+    class docs
+    '''
+	
+    def __init__(self):
+        '''
+        init docs
+        '''
+        # Init.
+        ScrolledWindow.__init__(self, 0, 0)
+        self.module_dir = os.path.join(os.path.dirname(__file__), "modules")        
+        
+        # Init widgets.
+        self.align = gtk.Alignment()
+        self.align.set(0.5, 0.5, 1, 1)
+        self.align.set_padding(0, 0, 2, 2)
+        self.layout = gtk.VBox()        
+        self.first_iconview = IconView()
+        self.second_iconview = IconView()
+        self.third_iconview = IconView()
+        self.extend_iconview = IconView()
+        self.first_iconview_scrolledwindow = ScrolledWindow()
+        self.second_iconview_scrolledwindow = ScrolledWindow()
+        self.third_iconview_scrolledwindow = ScrolledWindow()
+        self.extend_iconview_scrolledwindow = ScrolledWindow()
+        
+        # Connect widgets.
+        self.add_child(self.align)
+        self.align.add(self.layout)
+        
+        # Init icon lists.
+        self.all_modules = filter(lambda module_name: os.path.isdir(os.path.join(self.module_dir, module_name)), os.listdir(self.module_dir))        
+        self.first_modules = ["screen", "sound", "individuation", "date_time", "power"]
+        self.second_modules = ["keyboard", "mouse", "touchpad", "printer", "network", "bluetooth", "driver"]
+        self.third_modules = ["account", "auxiliary", "application_associate", "system_information"]
+        self.extend_modules = list(set(self.all_modules) - set(self.first_modules) - set(self.second_modules) - set(self.third_modules))
+        
+        # Add icons.
+        self.add_modules(self.first_modules, self.first_iconview, self.first_iconview_scrolledwindow)
+        self.add_modules(self.second_modules, self.second_iconview, self.second_iconview_scrolledwindow)
+        self.add_modules(self.third_modules, self.third_iconview, self.third_iconview_scrolledwindow)
+        self.add_modules(self.extend_modules, self.extend_iconview, self.extend_iconview_scrolledwindow)
+            
+    def add_modules(self, modules, icon_view, scrolled_window):
+        if len(modules) > 0:    
+            items = []
+            for module_name in modules:
+                items.append(IconItem(os.path.join(self.module_dir, module_name)))
+            
+            icon_view.add_items(items)    
+            scrolled_window.add_child(icon_view)
+            self.layout.pack_start(scrolled_window, True, True)
+        
+gobject.type_register(NavigatePanel)
+
+
+class IconItem(gobject.GObject):
+    '''
+    Icon item.
+    '''
+	
+    __gsignals__ = {
+        "redraw-request" : (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ()),
+    }
+    
+    def __init__(self, module_path):
+        '''
+        Initialize ItemIcon class.
+        
+        @param pixbuf: Icon pixbuf.
+        '''
+        gobject.GObject.__init__(self)
+        self.module_path = module_path
+        self.module_config = Config(os.path.join(self.module_path, "config.ini"))
+        self.module_config.load()
+        self.module_name = self.module_config.get("name", "zh_CN")
+        self.icon_pixbuf = gtk.gdk.pixbuf_new_from_file(os.path.join(self.module_path, self.module_config.get("icon", "icon")))
+        self.hover_flag = False
+        self.highlight_flag = False
+        
+    def emit_redraw_request(self):
+        '''
+        Emit `redraw-request` signal.
+        
+        This is IconView interface, you should implement it.
+        '''
+        self.emit("redraw-request")
+        
+    def get_width(self):
+        '''
+        Get item width.
+        
+        This is IconView interface, you should implement it.
+        '''
+        return 100
+        
+    def get_height(self):
+        '''
+        Get item height.
+        
+        This is IconView interface, you should implement it.
+        '''
+        return 100
+    
+    def render(self, cr, rect):
+        '''
+        Render item.
+        
+        This is IconView interface, you should implement it.
+        '''
+        icon_width = self.icon_pixbuf.get_width()
+        icon_height = self.icon_pixbuf.get_height()
+        
+        draw_pixbuf(
+            cr, 
+            self.icon_pixbuf,
+            rect.x + (rect.width - icon_width) / 2,
+            rect.y + (rect.height - icon_height) / 2)
+        
+        draw_text(cr, self.module_name, rect.x, rect.y + icon_height - DEFAULT_FONT_SIZE, rect.width, rect.height, alignment=pango.ALIGN_CENTER)
+        
+    def icon_item_motion_notify(self, x, y):
+        '''
+        Handle `motion-notify-event` signal.
+        
+        This is IconView interface, you should implement it.
+        '''
+        self.hover_flag = True
+        
+        self.emit_redraw_request()
+        
+    def icon_item_lost_focus(self):
+        '''
+        Lost focus.
+        
+        This is IconView interface, you should implement it.
+        '''
+        self.hover_flag = False
+        
+        self.emit_redraw_request()
+        
+    def icon_item_highlight(self):
+        '''
+        Highlight item.
+        
+        This is IconView interface, you should implement it.
+        '''
+        self.highlight_flag = True
+
+        self.emit_redraw_request()
+        
+    def icon_item_normal(self):
+        '''
+        Set item with normal status.
+        
+        This is IconView interface, you should implement it.
+        '''
+        self.highlight_flag = False
+        
+        self.emit_redraw_request()
+    
+    def icon_item_button_press(self, x, y):
+        '''
+        Handle button-press event.
+        
+        This is IconView interface, you should implement it.
+        '''
+        pass        
+    
+    def icon_item_button_release(self, x, y):
+        '''
+        Handle button-release event.
+        
+        This is IconView interface, you should implement it.
+        '''
+        pass
+    
+    def icon_item_single_click(self, x, y):
+        '''
+        Handle single click event.
+        
+        This is IconView interface, you should implement it.
+        '''
+        pass
+
+    def icon_item_double_click(self, x, y):
+        '''
+        Handle double click event.
+        
+        This is IconView interface, you should implement it.
+        '''
+        pass
+    
+    def icon_item_release_resource(self):
+        '''
+        Release item resource.
+
+        If you have pixbuf in item, you should release memory resource like below code:
+
+        >>> del self.pixbuf
+        >>> self.pixbuf = None
+
+        This is IconView interface, you should implement it.
+        
+        @return: Return True if do release work, otherwise return False.
+        
+        When this function return True, IconView will call function gc.collect() to release object to release memory.
+        '''
+        return False
+        
+gobject.type_register(IconItem)
