b4e03d48e9b7e87f3fcd22b6566796107c2abe09
yilang2007lw <yilang2007lw@gmail.com>
2012-11-01 11:21:31 +0800

    update agent: before checkout master

--- modules/network/src/nmlib/nm_remote_connection.py
+++ modules/network/src/nmlib/nm_remote_connection.py
@@ -73,11 +73,13 @@ class NMRemoteConnection(NMObject, NMConnection):
 
     def guess_secret_info(self):
         '''guess_secret_info'''
+        TypeConvert.dbus2py(self.settings_dict)
         self.secret_setting_name = ""
         self.secret_method = ""
 
     def update(self):
         try:
+            self.guess_secret_info()
             secret_agent.agent_save_secrets(self.object_path, self.secret_setting_name, self.secret_method)
             self.dbus_interface.Update(self.settings_dict, reply_handler = self.update_finish, error_handler = self.update_error)
             print secret_agent.agent_get_secrets(self.object_path, self.secret_setting_name, self.secret_method)
