028bac5036af2b33d74001737f6a71004f7a76e6
yilang2007lw <yilang2007lw@gmail.com>
2012-11-22 20:20:46 +0800

    update basic system info

--- modules/system_information/sysinfo.py
+++ modules/system_information/sysinfo.py
@@ -21,77 +21,84 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import GLib
+import platform
 import gtop
-import subprocess
+import os
+import socket
+import dbus
+import traceback
+
+system_bus = dbus.SystemBus()
+hostname_object = system_bus.get_object("org.freedesktop.hostname1", "/org/freedesktop/hostname1")
+hostname_interface = dbus.Interface(hostname_object, "org.freedesktop.hostname1")
 
 class SysInfo(object):
 
     def __init__(self):
-        self.cpu_info = self.get_cpu_info()
-        self.mem_info = self.get_memory_info()
-        self.sys_info = self.get_sys_info()
+        self.cpu_info = self._get_cpu_info()
+        self.mem_info = self._get_memory_info()
+        self.sys_info = self._get_sys_info()
+        self.mountlist = self._get_mountlist_info()
 
     def get_os_type(self):
-        if GLib.SIZEOF_VOID_P == 8:
-            return 64
-        else:
-            return 32
+        return platform.architecture()[0]
 
-    def get_cpu_info(self):
+    def _get_cpu_info(self):
         return gtop.cpu().dict()
 
-    def get_memory_info(self):
+    def _get_memory_info(self):
         return gtop.mem().dict()
 
     def get_memory_total(self):
         if "total" in self.mem_info.iterkeys():
             return self.mem_info["total"]
         else:
-            pass
+            return filter(lambda x: x.isdigit(), os.popen("grep MemTotal /proc/meminfo").read())
 
-    def get_sys_info(self):
+    def _get_sys_info(self):
         return gtop.sysinfo()
 
     def get_sys_info_ncpu(self):
-        return len(self.sys_info)
+        if len(self.sys_info):
+            return len(self.sys_info)
+        else:
+            return os.popen('cat /proc/cpuinfo |grep "processor" | wc -l').read().strip()
 
-    def get_sys_info_model_name(self):
+    def get_cpu_model_name(self):
         if "model name" in self.sys_info[0]:
             return self.sys_info[0]["model name"]
         else:
-            pass
+            return os.popen("grep 'model name' /proc/cpuinfo | head -1").read().split(":")[-1].strip()
 
     def get_hostname(self):
-        pass
+        return socket.gethostname()
 
     def set_hostname(self, hostname):
+        try:
+            hostname_interface.SetStaticHostname(hostname, True)
+        except:
+            traceback.print_exc()
+
+    def _get_current_display(self):
         pass
 
     def get_graphics_info(self):
         try:
-            process = subprocess.Popen(("glxinfo", "-l"), stdout = subprocess.PIPE)
-            process.wait()
-            glxinfo = process.stdout.readlines()
-
-            for item in glxinfo:
-                if item.startswith("OpenGL renderer string"):
-                    graphics_str = item.split(":")[-1]
-                else:
-                    continue
-            else:
-                # need read from /var/log/Xorg.0.log
-                # the '0' is read from env
-                pass
-
+            graphics_str = os.popen("glxinfo -l |grep 'OpenGL renderer string' ").read().split(":")[-1].strip()
         except:            
             pass
 
         return graphics_str
 
+    def _get_mountlist_info(self):
+        return gtop.mountlist()
 
     def get_mount_disk_size(self):
         pass
+
+    def get_disk_size(self):
+        st = os.statvfs(".")
+        return st.f_blocks * st.f_bsize / (1024 * 1024 * 1024)
 
 if __name__ == "__main__":
     sysinfo = SysInfo()
@@ -99,5 +106,7 @@ if __name__ == "__main__":
     # print sysinfo.get_os_type()
     # print sysinfo.get_memory_total()
     # print sysinfo.get_sys_info_ncpu()
-    # print sysinfo.get_sys_info_model_name()
+    # print sysinfo.get_cpu_model_name()
     print sysinfo.get_graphics_info()
+    # print sysinfo.get_hostname()
+    print sysinfo.get_disk_size()
