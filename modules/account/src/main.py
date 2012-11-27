1f60a9dcf47e7178db1602d82c40d1bfb635e26e
LongChangjin <admin@longchangjin.cn>
2012-11-27 18:24:10 +0800

    modify account

--- modules/account/src/main.py
+++ modules/account/src/main.py
@@ -32,7 +32,11 @@ from theme import app_theme
 from dtk.ui.label import Label
 from dtk.ui.button import CheckButton, Button
 from dtk.ui.new_entry import InputEntry
+from dtk.ui.combo import ComboBox
+from dtk.ui.new_slider import HSlider
 from dtk.ui.utils import propagate_expose, cairo_disable_antialias
+from treeitem import MyTreeView as TreeView
+from treeitem import MyTreeItem as TreeItem
 import gtk
 from module_frame import ModuleFrame
 
@@ -46,25 +50,31 @@ class AccountSetting(object):
         self.image_widgets = {}
         self.label_widgets = {}
         self.button_widgets = {}
-        self.adjust_widgets = {}
-        self.scale_widgets = {}
         self.alignment_widgets = {}
         self.container_widgets = {}
         self.view_widgets = {}
         self.dialog_widget = {}
+
+        self.__create_widget()
+        self.__adjust_widget()
+        self.__signals_connect()
     
     def __create_widget(self):
         # label 
+        self.label_widgets["account_name"] = Label("")
         self.label_widgets["account"] = Label(_("Account type"))
         self.label_widgets["passwd"] = Label(_("Password"))
+        self.label_widgets["passwd_char"] = Label("****")
         self.label_widgets["auto_login"] = Label(_("Automatic Login"))
         self.label_widgets["deepin_account_tips"] = Label(_("Deepin Account"))
-        self.label_widgets["account_type"] = Label("")
-        self.label_widgets["deepin_account"] = Label("")
+        self.label_widgets["deepin_account"] = Label(_("Unbound"))
+        self.label_widgets["account_name_new"] = Label(_("Account Name"))
+        self.label_widgets["account_type_new"] = Label(_("Account type"))
+        self.label_widgets["deepin_account_new"] = Label(_("Unbound"))
         # image
-        self.image_widgets["lock"] = gtk.image_new_from_file(
+        self.image_widgets["lock_pixbuf"] = gtk.gdk.pixbuf_new_from_file(
             app_theme.get_theme_file_path("image/set/lock.png"))
-        self.image_widgets["unlock"] = gtk.image_new_from_file(
+        self.image_widgets["unlock_pixbuf"] = gtk.gdk.pixbuf_new_from_file(
             app_theme.get_theme_file_path("image/set/unlock.png"))
         self.image_widgets["switch_bg_active"] = gtk.gdk.pixbuf_new_from_file(
             app_theme.get_theme_file_path("image/set/toggle_bg_active.png"))
@@ -74,7 +84,11 @@ class AccountSetting(object):
             app_theme.get_theme_file_path("image/set/toggle_fg.png"))
         self.image_widgets["set"] = gtk.gdk.pixbuf_new_from_file(
             app_theme.get_theme_file_path("image/set/set.png"))
+        self.image_widgets["account_ico"] = gtk.Image()
+        self.image_widgets["lock"] = gtk.Image()
         # button
+        self.button_widgets["account_name"] = InputEntry()
+        self.button_widgets["account_type"] = ComboBox([('Administrator', 0), ('Standard', 1)], max_width=115)
         self.button_widgets["auto_login"] = gtk.ToggleButton()
         self.button_widgets["passwd"] = InputEntry()
         self.button_widgets["net_access_check"] = CheckButton(_("网络访问权限"))
@@ -82,9 +96,105 @@ class AccountSetting(object):
         self.button_widgets["mountable_check"] = CheckButton(_("可加载移动设备"))
         self.button_widgets["disk_readwrite_check"] = CheckButton(_("磁盘操作权限完全"))
         self.button_widgets["backup_check"] = CheckButton(_("自动备份个人偏好设置并上传到云端，重新装机或在另一台计算机登录深度系统时您不再需要设置偏好。"))
-        self.button_widgets["binding"] = Label(_("提示：此功能需要绑定深度帐号。"))
+        self.button_widgets["binding"] = Label(_("提示：此功能需要绑定<span foreground=\"blue\" underline=\"single\">深度帐号。</span>"))
         self.button_widgets["add_account"] = Button(_("Add"))
         self.button_widgets["del_account"] = Button(_("Delete"))
+        self.button_widgets["account_create"] = Button(_("Create"))
+        self.button_widgets["account_cancle"] = Button(_("Cancle"))
+        self.button_widgets["account_type_new"] = ComboBox([('Administrator', 0), ('Standard', 1)], max_width=115)
+        self.button_widgets["net_access_check_new"] = CheckButton(_("网络访问权限"))
+        self.button_widgets["disk_readonly_check_new"] = CheckButton(_("磁盘操作权限只读"))
+        self.button_widgets["mountable_check_new"] = CheckButton(_("可加载移动设备"))
+        self.button_widgets["disk_readwrite_check_new"] = CheckButton(_("磁盘操作权限完全"))
+        self.button_widgets["backup_check_new"] = CheckButton(_("自动备份个人偏好设置并上传到云端，重新装机或在另一台计算机登录深度系统时您不再需要设置偏好。"))
+        self.button_widgets["binding_new"] = Label(_("提示：此功能需要绑定<span foreground=\"blue\" underline=\"single\">深度帐号。</span>"))
+        # container
+        self.container_widgets["slider"] = HSlider()
+        self.container_widgets["main_hbox"] = gtk.HBox(False)
+        self.container_widgets["left_vbox"] = gtk.VBox(False)
+        self.container_widgets["button_hbox"] = gtk.HBox(False)
+        self.container_widgets["right_vbox"] = gtk.VBox(False)
+        self.container_widgets["account_info_hbox"] = gtk.HBox(False)
+        self.container_widgets["account_add_vbox"] = gtk.VBox(False)
+        self.container_widgets["account_info_table"] = gtk.Table(5, 2)
+        self.container_widgets["check_button_table"] = gtk.Table(4, 2)
+        self.container_widgets["account_info_table_new"] = gtk.Table(4, 2)
+        self.container_widgets["check_button_table_new"] = gtk.Table(4, 2)
+        # treeview
+        self.view_widgets["account"] = TreeView()
+        # alignment
+        self.alignment_widgets["main_hbox"] = gtk.Alignment()
+        self.alignment_widgets["left_vbox"] = gtk.Alignment()
+        self.alignment_widgets["button_hbox"] = gtk.Alignment()
+        self.alignment_widgets["right_vbox"] = gtk.Alignment()
+        self.alignment_widgets["account_info_hbox"] = gtk.Alignment()
+        self.alignment_widgets["account_add_vbox"] = gtk.Alignment()
+        self.alignment_widgets["change_pswd"] = gtk.Alignment()
+    
+    def __adjust_widget(self):
+        self.container_widgets["slider"].append_page(self.alignment_widgets["main_hbox"])
+        self.container_widgets["slider"].append_page(self.alignment_widgets["change_pswd"])
+
+        self.alignment_widgets["main_hbox"].add(self.container_widgets["main_hbox"])
+        self.container_widgets["main_hbox"].pack_start(self.alignment_widgets["left_vbox"], False, False)
+        self.container_widgets["main_hbox"].pack_start(self.alignment_widgets["right_vbox"])
+        self.alignment_widgets["left_vbox"].add(self.container_widgets["left_vbox"])
+        self.alignment_widgets["right_vbox"].add(self.container_widgets["right_vbox"])
+        self.alignment_widgets["main_hbox"].set(0.5, 0.5, 1, 1)
+        self.alignment_widgets["main_hbox"].set_padding(5, 5, 5, 5)
+        # accounts list
+        self.container_widgets["left_vbox"].pack_start(self.view_widgets["account"])
+        self.container_widgets["left_vbox"].pack_start(self.container_widgets["button_hbox"], False, False, 10)
+        self.container_widgets["button_hbox"].pack_start(self.alignment_widgets["button_hbox"])
+        self.container_widgets["button_hbox"].pack_start(self.button_widgets["add_account"], False, False, 10)
+        self.container_widgets["button_hbox"].pack_start(self.button_widgets["del_account"], False, False)
+        self.alignment_widgets["button_hbox"].set(0, 0, 1, 1)
+        self.view_widgets["account"].set_size_request(325, 355)
+        # accounts info
+        self.alignment_widgets["right_vbox"].set(0, 0, 1, 1)
+        self.alignment_widgets["right_vbox"].set_padding(0, 0, 20, 20)
+        #self.container_widgets["right_vbox"].pack_start(self.container_widgets["account_info_vbox"], False, False)
+        self.container_widgets["right_vbox"].pack_start(self.container_widgets["account_info_table"], False, False)
+        self.container_widgets["right_vbox"].pack_start(self.container_widgets["check_button_table"], False, False)
+
+        self.container_widgets["account_info_table"].attach(self.image_widgets["account_ico"], 0, 1, 0, 1, 0)
+        self.container_widgets["account_info_table"].attach(self.alignment_widgets["account_info_hbox"], 0, 1, 0, 1, 0)
+        self.container_widgets["account_info_table"].attach(self.label_widgets["account"], 0, 1, 1, 2, 0)
+        self.container_widgets["account_info_table"].attach(self.button_widgets["account_type_new"], 1, 2, 1, 2, 0, 5, 10)
+        self.container_widgets["account_info_table"].attach(self.label_widgets["passwd"], 0, 1, 2, 3, 0)
+        self.container_widgets["account_info_table"].attach(self.label_widgets["passwd_char"], 1, 2, 2, 3, 0)
+        self.container_widgets["account_info_table"].attach(self.label_widgets["auto_login"], 0, 1, 3, 4, 0)
+        self.container_widgets["account_info_table"].attach(self.button_widgets["auto_login"], 1, 2, 3, 4, 0)
+        self.container_widgets["account_info_table"].attach(self.label_widgets["deepin_account_tips"], 0, 1, 4, 5, 0)
+        self.container_widgets["account_info_table"].attach(self.label_widgets["deepin_account"], 1, 2, 4, 5, 0)
+        self.button_widgets["auto_login"].set_size_request(49, 22)
+
+    def __signals_connect(self):
+        self.button_widgets["auto_login"].connect("expose-event", self.toggle_button_expose)
+
+    ######################################
+    # signals callback begine
+    # widget signals
+    def toggle_button_expose(self, button, event):
+        ''' toggle button expose'''
+        cr = button.window.cairo_create()
+        x, y, w, h = button.allocation
+        if button.get_active():
+            cr.set_source_pixbuf(
+                self.image_widgets["switch_bg_active"], x, y) 
+            cr.paint()
+            offet_x = self.image_widgets["switch_bg_active"].get_width() - self.image_widgets["switch_fg"].get_width()
+            cr.set_source_pixbuf(
+                self.image_widgets["switch_fg"], x+offet_x, y) 
+            cr.paint()
+        else:
+            cr.set_source_pixbuf(
+                self.image_widgets["switch_bg_nornal"], x, y) 
+            cr.paint()
+            cr.set_source_pixbuf(
+                self.image_widgets["switch_fg"], x, y) 
+            cr.paint()
+        return True
 
 if __name__ == '__main__':
     gtk.gdk.threads_init()
@@ -92,7 +202,10 @@ if __name__ == '__main__':
 
     account_settings = AccountSetting(module_frame)
     
-    #module_frame.add(account_settings.container_widgets["main_hbox"])
+    module_frame.add(account_settings.container_widgets["slider"])
+    module_frame.connect("realize", 
+        lambda w: account_settings.container_widgets["slider"].set_to_page(
+        account_settings.alignment_widgets["main_hbox"]))
     
     def message_handler(*message):
         (message_type, message_content) = message
