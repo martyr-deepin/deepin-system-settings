d73893f94f6b5820660a94be2cfc40775d8dab88
yilang2007lw <yilang2007lw@gmail.com>
2012-12-10 17:30:00 +0800

    add install shell

---
+++ modules/account/src/install_backend.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cp com.deepin.passwdservice.service /usr/share/dbus-1/system-services/ 
+echo "Copy .service file to /usr/share/dbus-1/system-services/"
+
+cp com.deepin.passwdservice.policy /usr/share/polkit-1/actions/
+echo "Copy .policy file to /usr/share/polkit-1/actions/"
+
+cp com.deepin.passwdservice.conf /etc/dbus-1/system.d/
+echo "Copy .conf file to /etc/dbus-1/system.d/"
+
+cp passwdservice.py /usr/lib/passwdservice.py
+echo "cp passwdservice.py"
+
+rm -f /usr/lib/passwdservice.py
+echo "remove passwdservice.py"
