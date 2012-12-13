4d6dd29a31e5c00d06710bc10a7ae94f2fd8692c
xiangzhai <xiangzhai83@gmail.com>
2012-12-13 15:38:18 +0800

    Init bluetooth module

---
+++ modules/bluetooth/src/bluetooth_view.py
@@ -0,0 +1,90 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012 Deepin, Inc.
+#               2012 Zhai Xiang
+# 
+# Author:     Zhai Xiang <zhaixiang@linuxdeepin.com>
+# Maintainer: Zhai Xiang <zhaixiang@linuxdeepin.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from dtk.ui.init_skin import init_skin
+from dtk.ui.utils import get_parent_dir
+import os
+
+app_theme = init_skin(
+    "deepin-bluetooth-settings", 
+    "1.0",
+    "01",
+    os.path.join(get_parent_dir(__file__, 2), "skin"),
+    os.path.join(get_parent_dir(__file__, 2), "app_theme"),
+    )
+
+from dtk.ui.label import Label
+from dtk.ui.combo import ComboBox
+from dtk.ui.button import ToggleButton
+from dtk.ui.constant import DEFAULT_FONT_SIZE, ALIGN_START, ALIGN_END
+from dtk.ui.utils import get_optimum_pixbuf_from_file
+import gobject
+import gtk
+
+class BlueToothView(gtk.VBox):
+    '''
+    class docs
+    '''
+	
+    def __init__(self):
+        '''
+        init docs
+        '''
+        gtk.VBox.__init__(self)
+
+        self.enable_align = self.__setup_align()
+        
+        '''
+        this->gtk.VBox pack_start
+        '''
+        self.__widget_pack_start(self, [self.enable_align])
+    
+    def __combo_item_selected(self, widget, item_text=None, item_value=None, item_index=None, object=None):
+        pass
+
+    def __setup_label(self, text="", width=50, align=ALIGN_END):
+        label = Label(text, None, DEFAULT_FONT_SIZE, align, width)
+        return label
+
+    def __setup_combo(self, items=[], width=120):
+        combo = ComboBox(items, None, 0, width)
+        return combo
+
+    def __setup_toggle(self):
+        toggle = ToggleButton(app_theme.get_pixbuf("inactive_normal.png"), 
+            app_theme.get_pixbuf("active_normal.png"))
+        return toggle
+
+    def __setup_align(self, xalign=0.5, yalign=0.5, xscale=1.0, yscale=1.0, 
+                      padding_top=20, padding_bottom=20, padding_left=20, padding_right=20):
+        align = gtk.Alignment()
+        align.set(xalign, yalign, xscale, yscale)
+        align.set_padding(padding_top, padding_bottom, padding_left, padding_right)
+        return align
+
+    def __widget_pack_start(self, parent_widget, widgets=[], expand=False, fill=False):
+        if parent_widget == None:
+            return
+        for item in widgets:
+            parent_widget.pack_start(item, expand, fill)
+
+gobject.type_register(BlueToothView)
