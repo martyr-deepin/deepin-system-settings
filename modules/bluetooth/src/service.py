994e61477fa56a49fc56817f7a4f68e8b65e5b11
yilang2007lw <yilang2007lw@gmail.com>
2012-12-13 17:28:29 +0800

    add service.py

---
+++ modules/bluetooth/src/service.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012 ~ 2013 Deepin, Inc.
+#               2012 ~ 2013 Long Wei
+#
+# Author:     Long Wei <yilang2007lw@gmail.com>
+# Maintainer: Long Wei <yilang2007lw@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from utils import BusBase
+
+class Service(BusBase):
+
+    def __init__(self, service_path):
+        BusBase.__init__(self, path = service_path , interface = "org.bluez.Characteristic")
+
+    def discovery_characteristics(self):
+        characteristics = self.dbus_method("DiscoveryCharacteristics")
+        if characteristics:
+            return map(lambda x:str(x), characteristics)
+        else:
+            return []
+
+    def get_properties(self):
+        return self.dbus_method("GetProperties")
+
+    def register_characteristics_watcher(self, watcher_path):
+        return self.dbus_method("RegisterCharacteristicsWatcher", watcher_path)
+
+    def unregister_characteristics_watcher(self, watcher_path):
+        return self.dbus_method("UnregisterCharacteristicsWatcher", watcher_path)
+
+if __name__ == "__main__":
+    pass
