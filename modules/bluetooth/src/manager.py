2f1a4adf9840ae571740ab10e4b70483a5cb04ec
yilang2007lw <yilang2007lw@gmail.com>
2012-12-13 15:38:30 +0800

    init bluetooth

---
+++ modules/bluetooth/src/manager.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012 ~ 2013 Deepin, Inc.
+#               2012 ~ 2013 Long Wei
+#
+# Author:     Long Wei <yilang2007lw@gmail.com>
+# Maintainer: Long Wei <yilang2007lw@gmail.com>
+#             Long Changjin <admin@longchangjin.cn>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gobject
+from utils import BusBase
+
+class Manager(BusBase):
+
+    __gsignals__  = {
+        "adapter-added":(gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (str,)),
+        "adapter-removed":(gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (str,)),
+        "default-adapter-changed":(gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (str,)),
+        "property-changed":(gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, (str, gobject.TYPE_PYOBJECT))
+            }
+
+    def __init__(self):
+        BusBase.__init__(self, path = "/", interface = "org.bluez.Manager")
+
+        self.bus.add_signal_receiver(self.adapter_added_cb, dbus_interface = self.object_interface, 
+                                     path = self.object_path, signal_name = "AdapterAdded")
+
+        self.bus.add_signal_receiver(self.adapter_removed_cb, dbus_interface = self.object_interface, 
+                                     path = self.object_path, signal_name = "AdapterRemoved")
+
+        self.bus.add_signal_receiver(self.default_adapter_changed_cb, dbus_interface = self.object_interface, 
+                                     path = self.object_path, signal_name = "DefaultAdapterChanged")
+
+        self.bus.add_signal_receiver(self.property_changed_cb, dbus_interface = self.object_interface, 
+                                     path = self.object_path, signal_name = "PropertyChanged")
+
+    def get_default_adapter(self):
+        return str(self.dbus_interface.DefaultAdapter())
+
+    def find_adapter(self, string):
+        return str(self.dbus_interface.FindAdapter(string))
+ 
+
+
+if __name__ == "__main__":
+    pass
---
+++ modules/bluetooth/src/utils.py
@@ -0,0 +1,108 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012 ~ 2013 Deepin, Inc.
+#               2012 ~ 2013 Long Wei
+#
+# Author:     Long Wei <yilang2007lw@gmail.com>
+# Maintainer: Long Wei <yilang2007lw@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import dbus
+import gobject
+import re
+import traceback
+
+from dbus.mainloop.glib import DBusGMainLoop
+DBusGMainLoop(set_as_default = True)
+
+name_re = re.compile("[0-9a-zA-Z-]*")
+
+system_bus = dbus.SystemBus()
+
+def valid_object_path(object_path):
+    if not isinstance(object_path, str):
+        return False
+
+    if not object_path.startswith("/"):
+        return False
+
+    return all(map(lambda x:name_re.match(x), object_path.split(".")))    
+
+class InvalidPropType(Exception):
+    
+    def __init__(self, prop_name, need_type, real_type = "string"):
+        self.prop_name = prop_name
+        self.need_type = need_type
+        self.real_type = real_type
+
+    def __str__(self):
+        return repr("property %s need type %s ,but given type is :%s",
+                    (self.prop_name, self.need_type, self.real_type))
+
+class InvalidObjectPath(Exception):
+    
+    def __init__(self, path):
+        self.path = path
+
+    def __str__(self):
+        return repr("InvalidObjectPath:" + self.path)
+
+class BusBase(gobject.GObject):
+    
+    def __init__(self, path, interface, service = "org.bluez", bus = system_bus):
+        gobject.GObject.__init__(self)
+        if valid_object_path(path):
+            self.object_path = path
+        else:
+            raise InvalidObjectPath(path)
+
+        self.object_interface = interface
+        self.service = service
+        self.bus = bus
+
+        try:
+            self.dbus_proxy = self.bus.get_object(self.service, self.object_path)
+            self.dbus_interface = dbus.Interface(self.dbus_proxy, self.object_interface)
+        except dbus.exceptions.DBusException:
+            traceback.print_exc()
+
+    def init_dbus_properties(self):        
+        try:
+            self.properties_interface = dbus.Interface(self.dbus_proxy, "org.freedesktop.DBus.Properties" )
+        except dbus.exceptions.DBusException:
+            traceback.print_exc()
+
+        if self.properties_interface:
+            try:
+                self.properties = self.properties_interface.GetAll(self.object_interface)
+            except:
+                print "get properties failed"
+                traceback.print_exc()
+
+    def dbus_method(self, method_name, *args, **kwargs):
+        try:
+            return apply(getattr(self.dbus_interface, method_name), args, kwargs)
+        except:
+            traceback.print_exc()
+
+    def call_async(self, method_name, *args, **kwargs):
+        try:
+            return apply(getattr(self.dbus_interface, method_name), args, kwargs)
+        except:
+            traceback.print_exc()
+
+if __name__ == "__main__":
+    pass
