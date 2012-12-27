ea65d611fa7272f5375be32ef2ab5bde949ee36a
xiangzhai <xiangzhai83@gmail.com>
2012-12-27 21:42:44 +0800

    Test from power.src.search_keywords import keywords

--- dss/search_page.py
+++ dss/search_page.py
@@ -20,10 +20,14 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# from dtk.ui.scrolled_window import ScrolledWindow
+from dtk.ui.utils import get_parent_dir
+import sys
+import os
 import gobject
 import gtk
 import xappy
+
+sys.path.append(os.path.join(get_parent_dir(__file__, 2), "modules"))
 
 class DeepinSearch():
     def __init__(self):
@@ -35,7 +39,6 @@ class DeepinSearch():
     def search_query(self, keywords):
         pass
 
-# class SearchPage(ScrolledWindow):
 class SearchPage(gtk.VBox):
     '''
     class docs
@@ -47,17 +50,17 @@ class SearchPage(gtk.VBox):
         '''
         gtk.VBox.__init__(self)
 
-        self.__keyword = []
+        self.__keywords = []
 
         self.__module_infos = module_infos
         '''
-        FIXME: try to import each module keywords
+        TODO: from EACH MODULE import keywords
+        '''
         for module_info_list in self.__module_infos:
             for module_info in module_info_list:
                 if module_info.search_keyword != "None":
-                    from .modules.power.src.search_keyword import keywords
+                    module = __import__("%s.src.search_keyword" % module_info.id, fromlist=["keywords"])
 
-                    print keywords
-        '''
+                    print module.keywords
 
 gobject.type_register(SearchPage)
