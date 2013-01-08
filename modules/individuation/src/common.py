bf9c4eef0592bf14f3cef425ba40bad5dfffa2d4
lovesnow <houshao55@gmail.com>
2013-01-08 16:26:53 +0800

    fix wallpapers is not vaild.

--- modules/individuation/src/common.py
+++ modules/individuation/src/common.py
@@ -257,7 +257,9 @@ def walk_images(root_dir, filter_type=["png", "jpeg"]):
                     split_content = content_type.split("/")
                     if len(split_content) == 2:
                         if split_content[0] == "image" and split_content[1] in filter_type:
-                            yield fil.get_path()
+                            file_path = fil.get_path()
+                            if not file_path.endswith(".part"):
+                                yield file_path
         except gio.Error, e: # why doesnt gio offer more-specific errors?
             print e
             logger.log_exception("Unhandled exception while walking on %s.", dir)
@@ -296,7 +298,9 @@ def gfile_is_image(gfile, filter_type=["png", "jpeg"]):
             split_content = content_type.split("/")
             if len(split_content) == 2:
                 if split_content[0] == "image" and split_content[1] in filter_type:
-                    return True
+                    file_path = gfile.get_path()
+                    if not file_path.endswith(".part"):
+                        return True
         return False        
     except:
         return False
