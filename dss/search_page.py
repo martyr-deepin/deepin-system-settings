4d106c3526fca3c468576dc21f591c98d810ee1f
xiangzhai <xiangzhai83@gmail.com>
2013-01-23 09:44:36 +0800

    Close split word, build index for production level dss

--- dss/search_page.py
+++ dss/search_page.py
@@ -32,7 +32,10 @@ import os
 import gobject
 import gtk
 import dbus
+'''
+TODO: production level do not need split word any more
 from split_word import init_jieba, split_word
+'''
 from theme import app_theme
 from constant import *
 import xappy
@@ -60,11 +63,14 @@ class KeywordSearch:
 
         self.__keywords = keywords
 
+        '''
+        TODO: production level do not need to init jieba any more
         init_jieba()
+        '''
 
+    '''
+    TODO: production level no need to build index any more
     def build_index(self, remove_old=True):
-        return
-
         if remove_old:
             remove_directory(SEARCH_DB_DIR)
 
@@ -89,9 +95,14 @@ class KeywordSearch:
                 self.__xappy.add(module_doc)
 
         self.__xappy.close()
+    '''
 
     def query(self, keyword):
+        return self.search_query([x for x in keyword.split(' ') if x.strip()])
+        '''
+        TODO: production level do not need to split word any more
         return self.search_query(list(split_word(keyword, True)))
+        '''
     
     def search_query(self, keywords):
         sconn = xappy.SearchConnection(SEARCH_DB_DIR)
@@ -150,9 +161,9 @@ class SearchPage(gtk.VBox):
         self.__keyword_search = KeywordSearch(self.__keywords)
         '''
         TODO: build index might be a heavey operation depend on keywords count
+              production level do not need to build index any more
+        BuildIndexThread(self).start()
         '''
-        #BuildIndexThread(self).start()
-
         self.scrolled_window.add_child(self.result_align)
         self.pack_start(self.scrolled_window)
 
