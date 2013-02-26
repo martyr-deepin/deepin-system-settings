e339ef348c4bb905134227ce24faa944f2dd2495
xiangzhai <xiangzhai83@gmail.com>
2013-02-26 14:13:34 +0800

    Update liuling`s desktop icon

--- modules/bluetooth/src/tray_bluetooth_plugin.py
+++ modules/bluetooth/src/tray_bluetooth_plugin.py
@@ -20,16 +20,20 @@
 # You should have received a copy of the GNU General Public License             
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from bt.manager import Manager
+from dtk.ui.label import Label
 import gtk
 
 class TrayBluetoothPlugin(object):
     def __init__(self):
-        pass
+        self.manager = Manager()
 
     def init_values(self, this_list):
         self.this = this_list[0]
         self.tray_icon = this_list[1]
         self.tray_icon.set_icon_theme("enable")
+        if self.manager.get_default_adapter() == "None":
+            self.tray_icon.set_visible(False)
 
     def id(slef):
         return "deepin-bluetooth-plugin-hailongqiu"
@@ -41,13 +45,29 @@ class TrayBluetoothPlugin(object):
         pass
 
     def plugin_widget(self):
-        return gtk.Button("BLUEZ")
+        plugin_align = self.__setup_align()
+        
+        return plugin_align
 
     def show_menu(self):
         self.this.set_size_request(160, 180)
 
     def hide_menu(self):
         pass
+
+    def __setup_align(self, 
+                      xalign=0, 
+                      yalign=0, 
+                      xscale=0, 
+                      yscale=0,                
+                      padding_top=0,                                 
+                      padding_bottom=0,                                            
+                      padding_left=0,                       
+                      padding_right=0):                                           
+        align = gtk.Alignment()                                                    
+        align.set(xalign, yalign, xscale, yscale)                                  
+        align.set_padding(padding_top, padding_bottom, padding_left, padding_right)
+        return align
 
 def return_plugin():
     return TrayBluetoothPlugin
