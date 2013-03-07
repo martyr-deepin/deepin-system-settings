30e36a1de93c76ac3bcba91a84145cff0dd7a3f2
xiangzhai <xiangzhai83@gmail.com>
2013-03-07 13:01:52 +0800

    Set background-duration min value to 1 minute for individuation module

--- modules/individuation/src/ui/detail_page.py
+++ modules/individuation/src/ui/detail_page.py
@@ -39,7 +39,8 @@ from nls import _
 import threading as td
 
 TIME_COMBO_ITEM =  [
-    (_("Never"), 0), ("10 %s" % _("Seconds"), 10), ("30 %s" % _("Seconds"), 30), 
+    (_("Never"), 0), 
+    #("10 %s" % _("Seconds"), 10), ("30 %s" % _("Seconds"), 30), 
     ("1 %s" % _("Minute"), 60), ("3 %s" % _("Minutes"), 180),
     ("5 %s" % _("Minutes"), 300), ("10 %s" % _("Minutes"), 600), 
     ("15 %s" % _("Minutes"), 900),("20 %s" % _("Minutes"), 1200), 
