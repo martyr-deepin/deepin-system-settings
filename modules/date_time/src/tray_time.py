c87310df7b8a61865eee6e4bf1bb6f4e62ac4b11
zhaixiang <zhaixiang@xzhai64.(none)>
2013-03-12 12:00:36 +0800

    Change system tray datetime interval to 1 second

--- modules/date_time/src/tray_time.py
+++ modules/date_time/src/tray_time.py
@@ -85,7 +85,7 @@ class TrayTime(gobject.GObject):
     def __update_time(self, timer):
         # modify interval.
         if self.__timer.Interval == 1:
-            self.__timer.Interval = 60000
+            self.__timer.Interval = 1000
         # emit event.
         self.emit("send-time", self.get_time(), self.__tray_time_hour_type, cn_check())
         
