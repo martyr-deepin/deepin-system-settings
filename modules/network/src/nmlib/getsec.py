aa8aea61ce707ed5091e840265a7a1767e186553
yilang <yilang2007lw@gmail.com>
2013-03-15 16:37:39 +0800

    network:add get wireless security by parse iwlist

---
+++ modules/network/src/nmlib/getsec.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2012 ~ 2013 Deepin, Inc.
+#               2012 ~ 2013 Long Wei
+#
+# Author:     Long Wei <yilang2007lw@gmail.com>
+# Maintainer: Long Wei <yilang2007lw@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from iwlistparse import parse_security
+from deepin_utils.process import get_command_output
+
+def get_ap_security(interface, address):
+    '''interface: wlan0 for wireless device
+       address: bssid of access point
+    '''
+    command = ["/sbin/iwlist"]
+    command.append(interface)
+    command.append("scan")
+
+    iwlist_output = map(lambda x: x.rstrip(), get_command_output(command))
+    
+    return parse_security(iwlist_output, address)
+
+if __name__ == "__main__":
+    print get_ap_security("wlan0", "5C:63:BF:7E:ED:64")
+
