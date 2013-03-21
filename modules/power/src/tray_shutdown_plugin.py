794a7c8bd60060daab6db75277726eb273dcb87c
long <long@long-U31Jg.(none)>
2013-03-21 17:54:01 +0800

    djkfdlf

--- modules/power/src/tray_shutdown_plugin.py
+++ modules/power/src/tray_shutdown_plugin.py
@@ -168,10 +168,15 @@ if __name__ == "__main__":
     dialog = TrayDialog()
     dialog.connect("hide", lambda w : gtk.main_quit())
     dialog.connect("destroy", lambda w : gtk.main_quit())
-    dialog.show_dialog("deepin_hibernate",
-                        LOGOUT_TOP_TEXT,
-                        LOGOUT_BOTTOM_TEXT)
-    dialog.run_exec = gui.cmd_dbus.logout
+    if len(sys.argv) >= 2:
+        if sys.argv[1] == 'shutdown':
+            dialog.show_dialog("deepin_shutdown")
+            dialog.run_exec = gui.cmd_dbus.new_stop
+        elif sys.argv[1] == 'logout':
+            dialog.show_dialog("deepin_hibernate",
+                                LOGOUT_TOP_TEXT,
+                                LOGOUT_BOTTOM_TEXT)
+            dialog.run_exec = gui.cmd_dbus.logout
     dialog.argv = 1
     dialog.set_bg_pixbuf(gtk.gdk.pixbuf_new_from_file('/usr/share/deepin-system-tray/src/image/on_off_dialog/deepin_on_off_bg.png'))
     dialog.show_pixbuf = gtk.gdk.pixbuf_new_from_file('/usr/share/deepin-system-tray/src/image/on_off_dialog/deepin_hibernate.png')
