8e0dfbed5c1070a07e9bfb95bceecb3064324401
Hualet Wang <mr.asianwang@gmail.com>
2013-09-13 17:16:22 +0800

    a little fix

--- modules/mount_media/src/tray_mount_gui.py
+++ modules/mount_media/src/tray_mount_gui.py
@@ -24,7 +24,7 @@ import sys
 import os
 from deepin_utils.file import get_parent_dir
 sys.path.append(os.path.join(get_parent_dir(__file__, 4), "dss"))
-
+from theme import app_theme
 from dtk.ui.dialog import ConfirmDialog
 from dtk.ui.line import HSeparator
 from vtk.draw  import draw_text, draw_pixbuf
